f73f62e6cada060f93c8d3b1ff12f6f4eae27336
root <root@localhost.localdomain>
2014-02-15 14:45:50 +0200

    The initial tcms core

---
+++ application/views/gen/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<title>TCMS</title>	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	
+	<!-- indlude fonts -->
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
+	
+	
+</head>
+
+
+<body>
---
+++ application/views/gen/logo.php
@@ -0,0 +1,3 @@
+<div id="logo" class="row">
+	<h1 class="col-md-8 col-md-offset-2"><span>A</span>utomation <span>T</span>emporary <span>C</span>lasifier</h1>
+</div>
