853a5ea81f3b4f5fedf915dcadc56349c64d1ca6
root <root@localhost.localdomain>
2014-02-15 15:15:01 +0200

    TCMS basic core

---
+++ application/views/gen/latest_activities.php
@@ -0,0 +1,13 @@
+<div id="log" class="col-md-3 col-md-offset-2">
+	<h2 class="log-title">
+		Latest Activities
+	</h2>
+	
+	<ul>
+		<li>"Westform" case has been closed by <b>Hazem</b> in 12/2/2014</li>
+		<li>"Westform" case has been opened by <b>Hazem</b> in 12/2/2014</li>
+		<li>"Westform" case has been opened by <b>Hazem</b> in 12/2/2014</li>
+		<li>"Westform" case has been opened by <b>Hazem</b> in 12/2/2014</li>
+		<li>"Westform" case has been opened by <b>Hazem</b> in 12/2/2014</li>
+	</ul>
+</div>
---
+++ application/views/gen/main_menu.php
@@ -0,0 +1,13 @@
+<div id="main-menu" class="row">
+	<ul class="col-md-8 col-md-offset-2">
+		<li><a href="#" class="active">Dashboard</a></li>
+		<li><a href="simple.html">Add Site</a></li>
+		<li><a href="#">Add Case</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Help</a></li>
+	</ul>
+	
+	<ul class="col-md-2">
+		<li>Hello, <a href="#" class="active">Hazem</a></li>				
+	</ul>
+</div>	
