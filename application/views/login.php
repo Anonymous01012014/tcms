ecb23d1e0211c4d4686049107015dccced15651f
molham <molham225@gmail.com>
2014-02-17 10:58:00 +0200

    Login initial view and form.

---
+++ application/views/login.php
@@ -0,0 +1,21 @@
+<div id="container" class="col-md-8 col-md-offset-2">
+	<h1 class="title">Login</h1>
+	<hr />
+	<form method="post" action="<?php echo base_url();?>login" id="login" >
+		<p>
+			<label for="username" id="username">User:</label>
+			<input type="text" name="username" id="username" />
+		</p>
+		<p>
+			<label for="password" id="password">Password:</label>
+			<input type="text" name="password" id="password" />
+		</p>	
+		<button type="submit" class="btn btn-default btn-lg" id="btn_login">
+			<span class="glyphicon glyphicon-ok"></span> Enter
+		</button>
+		</a>
+	</form>
+	
+
+</div>
+		
