ac446392a9c8df88c54b720e7dfa1a5b2aeb995f
Mohanad Kaleia <ms.kaleia@gmail.com>
2014-05-09 16:01:28 +0300

    correct typo error

--- application/views/gen/logo.php
+++ application/views/gen/logo.php
@@ -1,3 +1,3 @@
 <div id="logo" class="row">
-	<h1 class="col-md-8 col-md-offset-2"><span>A</span>utomation <span>T</span>emporary <span>C</span>lasifier</h1>
+	<h1 class="col-md-8 col-md-offset-2"><span>A</span>utomation <span>T</span>emporary <span>C</span>lassifier</h1>
 </div>
